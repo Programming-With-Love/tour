e3af489e13f8d3617df684af5d2888a6446531ba
Joeyscat <zhouyu.gg@qq.com>
2021-03-03 12:38:17 +0800

    json转struct功能补充: 读取json文件方式

--- cmd/json.go
+++ cmd/json.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/go-programming-tour-book/tour/internal/json2struct"
 
@@ -15,21 +17,46 @@ var jsonCmd = &cobra.Command{
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
+var jsonFilePath string
+
 var json2structCmd = &cobra.Command{
 	Use:   "struct",
 	Short: "json转换",
 	Long:  "json转换",
 	Run: func(cmd *cobra.Command, args []string) {
+		if jsonFilePath != "" {
+			var err error
+			str, err = readStr(jsonFilePath)
+			if err != nil {
+				log.Fatalf("read file error: %v", err)
+			}
+		}
+
 		parser, err := json2struct.NewParser(str)
 		if err != nil {
 			log.Fatalf("json2struct.NewParser err: %v", err)
 		}
 		content := parser.Json2Struct()
-		log.Printf("输出结果: %s", content)
+		log.Printf("输出结果: \n%s", content)
 	},
 }
 
 func init() {
 	jsonCmd.AddCommand(json2structCmd)
 	json2structCmd.Flags().StringVarP(&str, "str", "s", "", "请输入json字符串")
+	json2structCmd.Flags().StringVarP(&jsonFilePath, "file", "f", "", "请输入json文件路径")
+}
+
+func readStr(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	bs, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bs), nil
 }
